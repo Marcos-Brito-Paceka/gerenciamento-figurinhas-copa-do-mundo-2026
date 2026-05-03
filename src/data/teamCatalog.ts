ee2eba30f6518efd3c9eea4c1bc2029675c32909
marcos.brito <marcos.debrito@outlook.com>
2026-05-02 21:44:00 -0300

    fix: player and teamcatalog completed version

--- src/data/teamCatalog.ts
+++ src/data/teamCatalog.ts
@@ -1,8 +1,73 @@
 export const teamCatalog = [
-  ['brasil', 'Brasil', 'BRA'],
-  ['argentina', 'Argentina', 'ARG'],
-  ['franca', 'França', 'FRA'],
-  ['estados-unidos', 'Estados Unidos', 'USA'],
+  // Grupo A
   ['mexico', 'México', 'MEX'],
+  ['africa-do-sul', 'África do Sul', 'RSA'],
+  ['coreia-do-sul', 'Coreia do Sul', 'KOR'],
+  ['republica-tcheca', 'República Tcheca', 'CZE'],
+
+  // Grupo B
   ['canada', 'Canadá', 'CAN'],
-] as const
+  ['catar', 'Catar', 'QAT'],
+  ['bosnia', 'Bósnia e Herzegovina', 'BIH'],
+  ['suica', 'Suíça', 'SUI'],
+
+  // Grupo C
+  ['estados-unidos', 'Estados Unidos', 'USA'],
+  ['paraguai', 'Paraguai', 'PAR'],
+  ['australia', 'Austrália', 'AUS'],
+  ['turquia', 'Turquia', 'TUR'],
+
+  // Grupo D
+  ['brasil', 'Brasil', 'BRA'],
+  ['marrocos', 'Marrocos', 'MAR'],
+  ['haiti', 'Haiti', 'HAI'],
+  ['escocia', 'Escócia', 'SCO'],
+
+  // Grupo E
+  ['alemanha', 'Alemanha', 'GER'],
+  ['costa-do-marfim', 'Costa do Marfim', 'CIV'],
+  ['curacao', 'Curaçao', 'CUW'],
+  ['equador', 'Equador', 'ECU'],
+
+  // Grupo F
+  ['paises-baixos', 'Países Baixos', 'NED'],
+  ['japao', 'Japão', 'JPN'],
+  ['tunisia', 'Tunísia', 'TUN'],
+  ['suecia', 'Suécia', 'SWE'],
+
+  // Grupo G
+  ['belgica', 'Bélgica', 'BEL'],
+  ['egito', 'Egito', 'EGY'],
+  ['ira', 'Irã', 'IRN'],
+  ['nova-zelandia', 'Nova Zelândia', 'NZL'],
+
+  // Grupo H
+  ['espanha', 'Espanha', 'ESP'],
+  ['cabo-verde', 'Cabo Verde', 'CPV'],
+  ['arabia-saudita', 'Arábia Saudita', 'KSA'],
+  ['uruguai', 'Uruguai', 'URU'],
+
+  // Grupo I
+  ['franca', 'França', 'FRA'],
+  ['senegal', 'Senegal', 'SEN'],
+  ['iraque', 'Iraque', 'IRQ'],
+  ['noruega', 'Noruega', 'NOR'],
+
+  // Grupo J
+  ['argentina', 'Argentina', 'ARG'],
+  ['argelia', 'Argélia', 'ALG'],
+  ['austria', 'Áustria', 'AUT'],
+  ['jordania', 'Jordânia', 'JOR'],
+
+  // Grupo K
+  ['portugal', 'Portugal', 'POR'],
+  ['rd-congo', 'RD do Congo', 'COD'],
+  ['uzbequistao', 'Uzbequistão', 'UZB'],
+  ['colombia', 'Colômbia', 'COL'],
+
+  // Grupo L
+  ['inglaterra', 'Inglaterra', 'ENG'],
+  ['croacia', 'Croácia', 'CRO'],
+  ['gana', 'Gana', 'GHA'],
+  ['panama', 'Panamá', 'PAN'],
+] as const;
